c3a19496b5337351cd350983ebe4f435637e7faf
yangjun <yangjun@dinktalk.com>
2021-04-26 17:33:04 +0800

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-zero-example
+
+go 1.15
+
+require github.com/tal-tech/go-zero v1.1.6
---
+++ service/user/cmd/api/internal/svc/servicecontext.go
@@ -0,0 +1,20 @@
+package svc
+
+import (
+	"github.com/tal-tech/go-zero/core/stores/sqlx"
+	"go-zero-example/service/user/cmd/api/internal/config"
+	"go-zero-example/service/user/model"
+)
+
+type ServiceContext struct {
+	Config    config.Config
+	UserModel model.UserModel
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.Mysql.DataSource)
+	return &ServiceContext{
+		Config:    c,
+		UserModel: model.NewUserModel(conn, c.CacheRedis),
+	}
+}
